d5b62d1fdfb1eeb0cf1452bbace19c7d251029d3
Trần Tiến <101703006+Tiesen226@users.noreply.github.com>
2022-11-14 20:59:31 +0700

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,33 @@
+$(document).ready(
+    function () {
+        alert("Welcome to my KingdomCum!");
+    }
+)
+
+function fb() {
+    window.open('https://facebook.com/tien.nanoda.243', '_blank')
+}
+
+function tw() {
+    window.open('https://twitter.com/Tiesen226', '_blank')
+}
+
+function gh() {
+    window.open('https://github.com/Tiesen226', '_blank')
+}
+
+function vz() {
+    window.open('https://voz.vn/u/tiesen.1784070', '_blank')
+}
+
+function yt() {
+    window.open('https://youtube.com/@tiesen', '_blank')
+}
+
+function st() {
+    window.open('https://steamcommunity.com/profiles/76561199216867178', '_blank')
+}
+
+function dc() {
+    alert("Add my discord account to chat with me.");
+}
